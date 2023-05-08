aaa9c29223ad7dd1a82794b60156cdb127e92ff1
bruno-bueno <bbueno1122@gmail.com>
2023-05-08 09:58:36 -0300

    commit-aula

--- src/controllers/salaController.js
+++ src/controllers/salaController.js
@@ -1,3 +1,8 @@
-exports.get=async(req,res)=>{
-    return{"status":"OK","controller":"Sala"};
+// exports.get=async(req,res)=>{
+//     return{"status":"OK","controller":"Sala"};
+// }
+
+exports.get=()=>{
+    let salaModel = require('../models/salaModel')
+    return salaModel.listarSalas;
 }
--- src/models/salaModel.js
+++ src/models/salaModel.js
@@ -4,8 +4,24 @@ function listarSalas(){
             "_id":{
                 "$oid":""
             },
-            "nome":"guerreiros sa InfoCimol",
+            "nome":"guerreiros da InfoCimol",
             "tipo":"publica"
+        },{
+            "_id":{
+                "$oid":""
+            },
+            "nome":"Só os confirmados da info",
+            "tipo":"privada",
+            "chave": ""
+        },{
+            "_id":{
+                "$oid":""
+            },
+            "nome":"Guerreiros da info",
+            "tipo":"publica"
+
         }
     ]
 }
+
+module.exports={listarSalas}
